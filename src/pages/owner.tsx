b0af684014180d593adaea7e332f54d2778a00b9
LordRonz <aarontanhar2000@gmail.com>
2022-01-23 14:46:00 +0700

    feat: add logout functionality

--- src/pages/owner.tsx
+++ src/pages/owner.tsx
@@ -1,14 +1,20 @@
+import axios from 'axios';
 import { withIronSessionSsr } from 'iron-session/next';
 import type { GetServerSideProps, NextPage } from 'next';
-import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import React, { ReactNode } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 import Accent from '@/components/Accent';
+import Button from '@/components/buttons/Button';
 import MessageCard from '@/components/contents/MessageCard';
 import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
 import { getMessages } from '@/lib/fauna';
 import type { UserSession } from '@/pages/api/login';
 import type { Message } from '@/types/fauna';
+
+const toastStyle = { background: '#333', color: '#eee' };
 
 type OwnerPageProp = {
   user: UserSession;
@@ -17,16 +23,37 @@ type OwnerPageProp = {
 };
 
 const Owner: NextPage<OwnerPageProp> = ({ user, messages }) => {
+  const router = useRouter();
+
+  const handleLogout = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    toast.promise(axios.post('/api/logout', {}), {
+      loading: 'Loading...',
+      success: () => {
+        setTimeout(() => router.push('/login'), 2000);
+        return 'Logged out !, sayonara!';
+      },
+      error: (e) => {
+        if (axios.isAxiosError(e)) {
+          return e.response?.data.message ?? e.message;
+        }
+        return 'Logout failed, yo wtf is wrong';
+      },
+    });
+  };
+
   return (
     <>
       <Seo templateTitle='Me lord' />
       <main>
         <section className='bg-black text-primary-50'>
           <div className='layout flex flex-col justify-center items-center min-h-screen text-center gap-y-20'>
-            <div className='flex mt-4'>
+            <div className='flex mt-4 justify-evenly gap-4 w-full flex-wrap'>
               <h1 className='text-primary-100'>
                 Welcome, <Accent>{user.name}</Accent>
               </h1>
+              <Button onClick={handleLogout}>Logout</Button>
             </div>
             <div className='flex gap-4 grow-0 shrink flex-wrap items-center justify-evenly'>
               {messages.map(({ message }, i) => (
@@ -41,6 +68,17 @@ const Owner: NextPage<OwnerPageProp> = ({ user, messages }) => {
           </div>
         </section>
       </main>
+      <Toaster
+        toastOptions={{
+          style: toastStyle,
+          loading: {
+            iconTheme: {
+              primary: '#eb2754',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
     </>
   );
 };
