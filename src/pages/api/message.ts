845b36e74067d7709de25cdb54d7d4ff8bdc60f5
LordRonz <aarontanhar2000@gmail.com>
2022-02-11 17:18:47 +0700

    feat: add useRQWithToast hook

---
+++ src/pages/api/message.ts
@@ -0,0 +1,32 @@
+// pages/api/message.ts
+
+import { withIronSessionApiRoute } from 'iron-session/next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { getMessages } from '@/lib/fauna';
+
+export default withIronSessionApiRoute(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+      const user = req.session.user;
+
+      if (!user || user?.admin !== true) {
+        return res.status(401).send({ message: 'Unauthorized' });
+      }
+
+      const messages = await getMessages();
+
+      res.status(200).json({ messages });
+    } else {
+      res.status(405).json({ message: 'Method Not Allowed' });
+    }
+  },
+  {
+    cookieName: 'anjay_kue',
+    password: process.env.COOKIE_PASS as string,
+    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+    cookieOptions: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  }
+);
