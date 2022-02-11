74af28858a0a9eff973398e55b24e69d63fe2898
LordRonz <aarontanhar2000@gmail.com>
2022-02-11 12:10:37 +0700

    fix: message type error

--- src/lib/fauna.ts
+++ src/lib/fauna.ts
@@ -1,13 +1,19 @@
 import faunadb, { query as q } from 'faunadb';
 
-import type { AllMessagesRes, MessageRes, User, UserRes } from '@/types/fauna';
+import type {
+  AllMessagesRes,
+  Message,
+  MessageRes,
+  User,
+  UserRes,
+} from '@/types/fauna';
 
 const faunaClient = new faunadb.Client({
   secret: process.env.FAUNA_SECRET as string,
   domain: 'db.us.fauna.com',
 });
 
-export const createMessage = async (body: MessageRes) => {
+export const createMessage = async (body: Message) => {
   const data = await faunaClient.query<MessageRes>(
     q.Create(q.Collection('messages'), { data: body })
   );
